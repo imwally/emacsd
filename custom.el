68fc9e4ab13372301e6e5d1b266bbe98f3c19458
Wally <wally@imwally.net>
2015-03-21 23:37:01 -0400

    add packages and customisations

---
+++ custom.el
@@ -0,0 +1,6 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("8453c6ba2504874309bdfcda0a69236814cefb860a528eb978b5489422cb1791" "c0dd134ecd6ede6508c30f7d4ac92334229531df62284fc6572f65b4d0cde43f" default))))
--- init.el
+++ init.el
@@ -1,6 +1,19 @@
+;; load custom stuff
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 ;; make some room
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
 ;; go away
 (setq inhibit-splash-screen t)
+
+;; packages
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
+
+;; make it pretty
+(load-theme 'obsidian)
