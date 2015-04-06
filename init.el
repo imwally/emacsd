5c59063065d0b0590bd9f4a54edd4b02828fbbfd
Wally <wally@imwally.net>
2015-04-06 12:17:41 -0400

    add date functions

--- init.el
+++ init.el
@@ -14,3 +14,15 @@
 
 ;; make it pretty
 (load-theme 'spacegray)
+
+;; date and timestamp courtesy of kyle
+(defun k-insert-timestamp ()
+  (interactive)
+  (insert (format-time-string "%Y-%m-%dT%H:%M:%S%z")))
+
+(defun k-insert-date ()
+  (interactive)
+  (insert (format-time-string "%Y-%m-%d")))
+
+(global-set-key (kbd "C-c t t") 'k-insert-timestamp)
+(global-set-key (kbd "C-c t d") 'k-insert-date)
