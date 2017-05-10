8ff48cb26e26159874d28e75c3545bb9b3caa30a
Wally Jones <wally@imwally.net>
2017-05-10 19:56:07 -0400

    Remove marmalade repo and move packages.el to lib

--- lib/packages.el
+++ lib/packages.el
@@ -1,5 +1,4 @@
 ;; packages
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
