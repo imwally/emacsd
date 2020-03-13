4fa243ba55709594a6d605819132026f4e19cc6f
Wally Jones <wally@wallyjones.com>
2020-03-13 19:46:18 -0400

    Use gopls for autocomplete, auto imports, and auto formatting

--- lib/go.el
+++ lib/go.el
@@ -1,10 +1,24 @@
-;; Run gofmt before saving
-(setq gofmt-command "goimports")
-(add-hook 'before-save-hook 'gofmt-before-save)
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred))
 
-;; go autocomplete
-(require 'go-autocomplete)
-(require 'auto-complete-config)
-(ac-config-default)
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
 
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
 
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1))
+
+(use-package company-lsp
+     :ensure t
+     :commands company-lsp)
+	
