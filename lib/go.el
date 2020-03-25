48c3d80274ec70a4e8b07e05c970790678f12d0b
Wally Jones <wally@wallyjones.com>
2020-03-24 21:09:35 -0400

    Add comments about packages being used

--- lib/go.el
+++ lib/go.el
@@ -1,23 +1,30 @@
+;; Language Server Protocol mode
 (use-package lsp-mode
   :ensure t
   :commands (lsp lsp-deferred)
   :hook (go-mode . lsp-deferred))
 
+;; Format code and add/delete imports before saving
 (defun lsp-go-install-save-hooks ()
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
 
+;; Fancy UI overlays
+;; https://github.com/emacs-lsp/lsp-ui
 (use-package lsp-ui
   :ensure t
   :commands lsp-ui-mode)
 
+;; Auto completion
+;; http://company-mode.github.io
 (use-package company
   :ensure t
   :config
   (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 1))
 
+;; Use company with lsp
 (use-package company-lsp
   :ensure t
   :commands company-lsp)
