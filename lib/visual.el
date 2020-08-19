4428b60bfb10e57362075f6844239a98360cd14d
Wally Jones <wally@wallyjones.com>
2020-08-19 17:01:18 -0400

    Show line numbers

--- lib/visual.el
+++ lib/visual.el
@@ -10,6 +10,9 @@
 ;; don't wrap lines by default
 (set-default 'truncate-lines t)
 
+;; give me line numbers
+(global-display-line-numbers-mode 1)
+
 ;; make it pretty
 (load-theme 'monotropic t)
 (set-face-attribute 'default nil :font "SF Mono-11")
