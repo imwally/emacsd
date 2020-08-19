8d8a5a487973bc4b975ef467d1f304af1b695c4a
Wally Jones <wally@wallyjones.com>
2020-08-19 17:02:22 -0400

    Use file extension for packages.el

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 		   "cl.el"
 		   "go.el"
 		   "magit.el"
-		   "packages"
+		   "packages.el"
 		   "scp-paste.el"
 		   "util.el"
 		   "visual.el"
@@ -36,7 +36,7 @@
     ("f738c3eb5cfc7e730fea413f9cd8ba0624bd8b4837451660fe169f13f77c7814" "30289fa8d502f71a392f40a0941a83842152a68c54ad69e0638ef52f04777a4c" default)))
  '(package-selected-packages
    (quote
-    (company-lsp lsp-ui use-package lsp-mode monotropic-theme plan9-theme exec-path-from-shell scpaste magit go-mode slime))))
+    (yaml-mode web-mode plisp-mode company-lsp lsp-ui use-package lsp-mode monotropic-theme plan9-theme exec-path-from-shell scpaste magit go-mode slime))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
